783d98157029ba747762401cc68585c436f659bb
kevfallon97 <kfallon@tcd.ie>
2020-10-08 19:24:13 +0100

    added covariance and correlation calculations

--- section_12_measuring_risk/calculating_risk.py
+++ section_12_measuring_risk/calculating_risk.py
@@ -23,4 +23,18 @@ sec_returns = np.log(sec_data / sec_data.shift(1))
 print(sec_returns[['PG', 'BEI.DE']].mean() * 250)
 print(sec_returns[['PG', 'BEI.DE']].std() * 250 ** 0.5)
 
-# print(sec_returns[['PG', 'BEI.DE']])
+# COVARIANCE AND CORRELATION
+
+# annualised variance
+PG_var_a = sec_returns['PG'].var() * 250
+BEI_var_a = sec_returns['BEI.DE']. var() * 250
+
+# annualised covariance matrix
+cov_matrix = sec_returns.cov() * 250
+print(f"COVARIANCE MATRIX \n{cov_matrix}")
+
+# calculate the correlation 
+corr_matrix = sec_returns.corr()
+print(f"CORRELATION MATRIX\n{corr_matrix}")
+# NOTE: THIS IS THE CORRELATION BETWEEN RETURNS, NOT THE EQUITY PRICES
+# NOTE: DO NOT ANNUALISE THE CORRELATION TABLE, IT DOES NOT CONTAIN AVERAGE DAILY VALUES
