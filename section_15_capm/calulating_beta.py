b3fb70af657528dcd19eb48d6fc48b5be439b0f2
kevfallon97 <kfallon@tcd.ie>
2020-10-10 14:21:17 +0100

    calculated the expected return of PG using the CAPM

--- section_15_capm/calulating_beta.py
+++ section_15_capm/calulating_beta.py
@@ -21,5 +21,11 @@ cov_with_market = cov.iloc[0,1]
 # calculate the market variance, the denominatior in the beta calculation
 market_var = sec_returns['^GSPC'].var() * 250
 
+# calculate beta
 PG_beta = cov_with_market / market_var
 print(f"PG Beta: {PG_beta}")
+
+# calculate the expected return using the CAPM
+# risk-free rate = 2.5%, risk premium = 5%
+PG_er = 0.025 + PG_beta * 0.05
+print(f"Expected Return: {PG_er}")
