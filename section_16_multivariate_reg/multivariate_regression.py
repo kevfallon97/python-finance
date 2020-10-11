df930a8c73e3c1720ff2b51690f6c6ee31d06619
kevfallon97 <kfallon@tcd.ie>
2020-10-11 18:46:51 +0100

    carried out section 16, multivariate regression

---
+++ section_16_multivariate_reg/multivariate_regression.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+data = pd.read_excel('Housing.xlsx')
+
+# MULTIVARIATE REGRESSION
+# independent variables: "House Size (sq.ft.)" , "Number of Rooms" , "Year of Construction"
+X = data[["House Size (sq.ft.)" , "Number of Rooms" , "Year of Construction"]]
+Y = data['House Price']
+
+X1 = sm.add_constant(X)
+reg = sm.OLS(Y, X1).fit()
+print(reg.summary())
+
+# independent variables: "House Size (sq.ft.)" , "Number of Rooms"
+X = data[["House Size (sq.ft.)" , "Number of Rooms"]]
+Y = data['House Price']
+
+X1 = sm.add_constant(X)
+reg = sm.OLS(Y, X1).fit()
+print(reg.summary())
+
+
+# independent variables: "House Size (sq.ft.)" , "Year of Construction"
+X = data[["House Size (sq.ft.)" , "Year of Construction"]]
+Y = data['House Price']
+
+X1 = sm.add_constant(X)
+reg = sm.OLS(Y, X1).fit()
+print(reg.summary())
