de80b5cad44336f1e28edcb7090b69d5f0725e9e
kevfallon97 <kfallon@tcd.ie>
2020-10-12 08:49:36 +0100

    added reg summary

--- section_16_multivariate_reg/multivariate_regression.py
+++ section_16_multivariate_reg/multivariate_regression.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 data = pd.read_excel('Housing.xlsx')
 
 # MULTIVARIATE REGRESSION
+
 # independent variables: "House Size (sq.ft.)" , "Number of Rooms" , "Year of Construction"
 X = data[["House Size (sq.ft.)" , "Number of Rooms" , "Year of Construction"]]
 Y = data['House Price']
@@ -14,6 +15,8 @@ Y = data['House Price']
 X1 = sm.add_constant(X)
 reg = sm.OLS(Y, X1).fit()
 print(reg.summary())
+
+# Perform additional regressions
 
 # independent variables: "House Size (sq.ft.)" , "Number of Rooms"
 X = data[["House Size (sq.ft.)" , "Number of Rooms"]]
@@ -26,6 +29,14 @@ print(reg.summary())
 
 # independent variables: "House Size (sq.ft.)" , "Year of Construction"
 X = data[["House Size (sq.ft.)" , "Year of Construction"]]
+Y = data['House Price']
+
+X1 = sm.add_constant(X)
+reg = sm.OLS(Y, X1).fit()
+print(reg.summary())
+
+# independent variables: "Number of Rooms" , "Year of Construction"
+X = data[["Number of Rooms" , "Year of Construction"]]
 Y = data['House Price']
 
 X1 = sm.add_constant(X)
