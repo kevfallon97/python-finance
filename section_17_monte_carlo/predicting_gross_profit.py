46311440651b42f7fa4a1fbb52b3d274d2c02063
kevfallon97 <kfallon@tcd.ie>
2020-10-12 09:00:28 +0100

    completed gross profit prediction (Part 2)

--- section_17_monte_carlo/predicting_gross_profit.py
+++ section_17_monte_carlo/predicting_gross_profit.py
@@ -22,7 +22,7 @@ plt.show()
 
 # assume that COGS are typically 60% of the revenues generated for the same period, m = 60% stdev = 10% 
 # assign a random cogs value to each one of the norm dist data pints
-COGS = - (rev * np.random.normal(0.6, 0.1))
+COGS = -(rev * np.random.normal(0.6, 0.1))
 
 plt.figure(figsize=(15,6))
 plt.plot(COGS)
@@ -30,3 +30,20 @@ plt.show()
 
 print(f"COGS mean: {COGS.mean()}")
 print(f"COGS stdev: {COGS.std()}")
+
+# Gross profit calculation
+gross_profit = rev + COGS #COGS is negative
+plt.figure(figsize=(15,6))
+plt.plot(gross_profit)
+plt.show()
+
+print("Gross Profit Figures")
+print(f"Max:\t{max(gross_profit)}")
+print(f"Min:\t{min(gross_profit)}")
+print(f"Mean:\t{gross_profit.mean()}")
+print(f"Stdev:\t{gross_profit.std()}")
+
+# plot data on a histogram
+plt.figure(figsize=(10,6))
+plt.hist(gross_profit, bins=20)
+plt.show()
